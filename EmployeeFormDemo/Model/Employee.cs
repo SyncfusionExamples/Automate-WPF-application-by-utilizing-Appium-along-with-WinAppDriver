2077e751f24894cd9e18f8d535087aef38c79286
vijayarasan <vijayarasan.sivanandham@syncfusiom.com>
2024-03-14 17:09:07 +0530

    ES-875077 Added the sample and images with content in ReadMe file.

---
+++ EmployeeFormDemo/Model/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EmployeeFormDemo
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        int employeeID;
+        string employeeName;
+        string employeeMail;
+        DateTime? employeeBirthDate;
+        string employeeGender;
+        
+        public int EmployeeID
+        {
+            get { return employeeID; }
+            set
+            {
+                employeeID = value;
+                OnPropertyChanged("EmployeeID");
+            }
+        }
+        
+        public string EmployeeName
+        {
+            get { return employeeName; }
+            set
+            {
+                employeeName = value;
+                OnPropertyChanged("EmployeeName");
+            }
+        }
+        
+        public string EmployeeMail
+        {
+            get { return employeeMail; }
+            set
+            {
+                employeeMail = value;
+                OnPropertyChanged("EmployeeMail");
+            }
+        }
+        
+        public DateTime? EmployeeBirthDate
+        {
+            get { return employeeBirthDate; }
+            set
+            {
+                employeeBirthDate = value;
+                OnPropertyChanged("employeeBirthDate");
+            }
+        }
+        
+        public string EmployeeGender
+        {
+            get { return employeeGender; }
+            set
+            {
+                employeeGender = value;
+                OnPropertyChanged("EmployeeGender");
+            }
+        }
+
+        public Employee()
+        {
+                
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
---
+++ EmployeeFormTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeFormTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeFormTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67e6c1a7-221c-44ac-91f3-03ae02c35d7a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
